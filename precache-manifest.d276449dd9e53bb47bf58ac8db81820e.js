eab54088aa2b5122ff5e19b4b4355c4391eaac71
smart-reports <50381700+smart-reports@users.noreply.github.com>
2020-06-27 17:57:16 -0500

    Updates

---
+++ precache-manifest.d276449dd9e53bb47bf58ac8db81820e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2357e0c60fcac2cb3fcbfc823c0bbfe",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6d4a0547a844f81049f4",
+    "url": "/monsters-rolodex/static/css/main.4ccc43c8.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d4a0547a844f81049f4",
+    "url": "/monsters-rolodex/static/js/main.7716822b.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
